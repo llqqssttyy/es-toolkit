fcec8fec3015b2e0835f8c007d38f560cc8c39aa
KIM DAEUN <midekuna@gmail.com>
2025-12-01 12:38:42 +0900

    feat: add logo from /docs workspace

--- playground/src/components/HomeButton.tsx
+++ playground/src/components/HomeButton.tsx
@@ -1,3 +1,9 @@
+import LightLogo from '@assets/logo_black.png';
+
 export function HomeButton() {
-  return <a href="/">Logo</a>;
+  return (
+    <a href="/">
+      <img src={LightLogo} alt="es-toolkit" className="h-4 w-auto" />
+    </a>
+  );
 }
