7866a2022d6e1075178d5399b0051d6f92c7cc88
KIM DAEUN <midekuna@gmail.com>
2025-12-01 22:01:15 +0900

    feat(ui): Change button style

--- playground/src/components/ui/button.tsx
+++ playground/src/components/ui/button.tsx
@@ -4,13 +4,12 @@ import { Slot } from '@radix-ui/react-slot';
 import { buttonVariants, type ButtonVariants } from './button.variants';
 
 export interface ButtonProps
-  extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'variant' | 'size' | 'color'>,
+  extends Omit<React.ComponentProps<'button'>, 'variant' | 'size' | 'color'>,
     ButtonVariants {
-  ref?: React.Ref<HTMLButtonElement>;
   asChild?: boolean;
 }
 
-export function Button({ ref, className, variant, size, color, asChild = false, ...props }: ButtonProps) {
+export function Button({ className, variant, size, color, asChild = false, ...props }: ButtonProps) {
   const Comp = asChild ? Slot : 'button';
-  return <Comp ref={ref} className={cn(buttonVariants({ variant, size, color, className }))} {...props} />;
+  return <Comp className={cn(buttonVariants({ variant, size, color }), className)} {...props} />;
 }
